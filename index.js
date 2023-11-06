1062662f752ccc09c5025703c0323c011f797206
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2023-11-06 10:59:09 -0700

    find array method lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(games){
+   const win = games.find ((game)=> game.result == "W")
+    if(win){
+        return win.year
+    } else {
+        return win
+    }
+}
